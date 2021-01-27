248f43a97e351c24f47250fd2c9052b4bb8ab9a5
zhangchunhui <zhangchunhui@huobi.com>
2021-01-27 14:35:51 +0800

    a
    a

--- microee-ethdix-fe/src/App.js
+++ microee-ethdix-fe/src/App.js
@@ -3,11 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          microee-ethdix-fe
-        </p>
-      </header>
+      <h1>
+        microee-ethdix-fe
+      </h1>
     </div>
   );
 }
